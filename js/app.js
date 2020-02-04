625b1bd8e66113b6d50fa1cdc6b721024e545466
disneyfan9184 <disneyfan9184@gmail.com>
2020-02-04 14:33:17 -0500

    added initial markup

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const container = document.querySelector('.container');
+const artistImage = document.querySelector('.artist');
+const media = document.querySelector('.media');
+const overlay = document.querySelector('.overlay');
+const searchElem = document.querySelector('#search');
+
+searchElem.addEventListener('keydown', event => {
+  if (event.key === 'Enter') {
+    getContent(searchElem.value);
+  }
+});
+
+const getContent = search => {
+  const url = new URL('https://itunes.apple.com/search');
+  const params = { term: search, media: 'musicVideo' };
+  url.search = new URLSearchParams(params);
+  fetch(url, { method: 'POST' })
+    .then(results => results.json())
+    .then(data => {
+      const resultsHTML = data.results
+        .map(
+          result => `
+        <div style="background-image: url(${result.artworkUrl100});"
+        onclick="openMedia('${result.previewUrl}', '${result.trackCensoredName}')"
+        class="result"></div>
+      `
+        )
+        .join('');
+      container.innerHTML = resultsHTML;
+    });
+};
